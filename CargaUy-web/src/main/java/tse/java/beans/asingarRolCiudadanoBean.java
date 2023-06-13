d1801ea63b44622816d593b2e8882af935f66a9a
Marcelo Alejandro Bravo Sugo <marceb95@gmail.com>
2023-06-13 02:26:24 -0300

    Fin asignar roles ciudadano

--- CargaUy-web/src/main/java/tse/java/beans/asingarRolCiudadanoBean.java
+++ CargaUy-web/src/main/java/tse/java/beans/asingarRolCiudadanoBean.java
@@ -7,6 +7,7 @@ import tse.java.entity.*;
 import tse.java.enumerated.RolCiudadano;
 import tse.java.enumerated.TipoUsuario;
 import tse.java.exception.RubroExisteException;
+import tse.java.persistance.IChoferDAO;
 import tse.java.persistance.ICiudadanoDAO;
 import tse.java.service.ICiudadanosService;
 import tse.java.service.IUsuariosService;
@@ -15,7 +16,10 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -43,64 +47,65 @@ public class asingarRolCiudadanoBean {
 
     @PostConstruct
     public void init(){
-        UsuarioDTO u2 = new UsuarioDTO("Rita", "Iglesias", LocalDate.now(), "rita.iglesias.adrover@gmail.com", "admin", "admin", TipoUsuario.ADMIN);
-        iUsuariosService.registrarUsuario(u2);
-        if(ciudadanoDAO.buscarCiudadanoPorId(1)==null){
-            Chofer c = new Chofer("marce","1234");
-            c.setAsignaciones(new ArrayList<Asignacion>());
-            ciudadanosService.agregarHijoCiudadano(c);
-        }
-        if(ciudadanoDAO.buscarCiudadanoPorId(2)==null) {
-            Ciudadano a = new Ciudadano("ciuda", "1111", new ArrayList<Asignacion>());
-            ciudadanosService.agregarCiudadano(a);
-        }
         listaCiudadanos = ciudadanoDAO.listarCiudadanos();
         listaCiudadanos.sort(Comparator.comparing(CiudadanoDTO::getCedula));
     }
 
-    public void onRowEdit(RowEditEvent<CiudadanoDTO> event) {
+    public void onRowEdit(RowEditEvent<CiudadanoDTO> event) throws IOException {
         CiudadanoDTO dtr = event.getObject();
         String msg = "Me pasaron para modificar el ciudadano con id " + dtr.getIdCiudadano() + " con el documento " + dtr.getCedula();
         Logger.getLogger(asingarRolCiudadanoBean.class.getName()).log(Level.INFO, msg);
         Ciudadano c = ciudadanoDAO.buscarCiudadanoPorId(dtr.getIdCiudadano());
         if(rol.equals("Chofer") && !(c instanceof Chofer)) {
-            Chofer ch = new Chofer(c.getEmail(),c.getCedula());
-            ch.setAsignaciones(new ArrayList<Asignacion>());
+            Chofer ch = new Chofer(c.getEmail(),c.getCedula(),new ArrayList<Asignacion>());
             if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario))
                 ciudadanosService.eliminarHijoCiudadano(c);
             else
                 ciudadanosService.eliminarCiudadano(c);
             ciudadanosService.agregarHijoCiudadano(ch);
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Ciudadano actualizado.");
-            PrimeFaces.current().dialog().showMessageDynamic(message);
+            recargarPagina();
         } else if(rol.equals("Responsable") && !(c instanceof Responsable)){
             Responsable r = new Responsable(c.getEmail(),c.getCedula());
-            r.setAsignaciones(new ArrayList<Asignacion>());
-            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario))
+            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario)) {
+                if (c instanceof Chofer) {
+                    Chofer ch = (Chofer) c;
+                    ch.setAsignaciones(new ArrayList<Asignacion>());
+                    ciudadanosService.modificarHijoCiudadano(ch);
+                }
                 ciudadanosService.eliminarHijoCiudadano(c);
-            else
+            } else {
                 ciudadanosService.eliminarCiudadano(c);
+            }
             ciudadanosService.agregarHijoCiudadano(r);
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Ciudadano actualizado.");
-            PrimeFaces.current().dialog().showMessageDynamic(message);
+            recargarPagina();
         } else if(rol.equals("Funcionario") && !(c instanceof Funcionario)){
             Funcionario f = new Funcionario(c.getEmail(),c.getCedula());
-            f.setAsignaciones(new ArrayList<Asignacion>());
-            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario))
+            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario)) {
+                if (c instanceof Chofer) {
+                    Chofer ch = (Chofer) c;
+                    ch.setAsignaciones(new ArrayList<Asignacion>());
+                    ciudadanosService.modificarHijoCiudadano(ch);
+                }
                 ciudadanosService.eliminarHijoCiudadano(c);
-            else
+            } else {
                 ciudadanosService.eliminarCiudadano(c);
+            }
             ciudadanosService.agregarHijoCiudadano(f);
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Ciudadano actualizado.");
-            PrimeFaces.current().dialog().showMessageDynamic(message);
-        } else if(rol.equals("Ciudadano") && !(c instanceof Ciudadano)){
-            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario))
+            recargarPagina();
+        } else if(rol.equals("Ciudadano") && ((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario))){
+            Ciudadano cnew = new Ciudadano(c.getEmail(),c.getCedula(),null);
+            if((c instanceof Chofer) || (c instanceof Responsable) || (c instanceof Funcionario)) {
+                if (c instanceof Chofer) {
+                    Chofer ch = (Chofer) c;
+                    ch.setAsignaciones(new ArrayList<Asignacion>());
+                    ciudadanosService.modificarHijoCiudadano(ch);
+                }
                 ciudadanosService.eliminarHijoCiudadano(c);
-            else
+            } else {
                 ciudadanosService.eliminarCiudadano(c);
-            ciudadanosService.agregarCiudadano(c);
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", "Ciudadano actualizado.");
-            PrimeFaces.current().dialog().showMessageDynamic(message);
+            }
+            ciudadanosService.agregarCiudadano(cnew);
+            recargarPagina();
         } else {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Se intenta modificar el ciudadano por el rol que ya tiene");
             PrimeFaces.current().dialog().showMessageDynamic(message);
@@ -128,15 +133,14 @@ public class asingarRolCiudadanoBean {
         this.rol = rol;
     }
 
-    public String getRolUsuario(CiudadanoDTO ciud){
-        Ciudadano c = ciudadanoDAO.buscarCiudadanoPorId(ciud.getIdCiudadano());
-        if(c instanceof Chofer)
-            return "Chofer";
-        else if(c instanceof Funcionario)
-            return "Funcionario";
-        else if(c instanceof Responsable)
-            return "Responsable";
-        else
-            return "Ciudadano";
+    private void recargarPagina(){
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+        try {
+            ec.redirect("/CargaUy-web/admin/asignarRoles.xhtml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        fc.responseComplete();
     }
 }
