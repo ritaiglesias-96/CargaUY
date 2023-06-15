b36431e3548deee9fcf352b4e7e77676fa1695f4
Maria Rita Iglesias <rita.iglesias.adrover@gmail.com>
2023-06-15 12:12:16 -0300

    Agregando el perisferico que simula PDI

---
+++ ProveedorEmpresas/src/main/java/empresa/ObjectFactory.java
@@ -0,0 +1,56 @@
+
+package empresa;
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the empresa package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: empresa
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetEmpresaRequest }
+     * 
+     */
+    public GetEmpresaRequest createGetEmpresaRequest() {
+        return new GetEmpresaRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetEmpresaResponse }
+     * 
+     */
+    public GetEmpresaResponse createGetEmpresaResponse() {
+        return new GetEmpresaResponse();
+    }
+
+    /**
+     * Create an instance of {@link Empresa }
+     * 
+     */
+    public Empresa createEmpresa() {
+        return new Empresa();
+    }
+
+}
---
+++ ProveedorEmpresas/src/main/java/tse/java/proveedorEmpresas/service/EmpresaEndpoint.java
@@ -0,0 +1,29 @@
+package tse.java.proveedorEmpresas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import empresa.GetEmpresaRequest;
+import empresa.GetEmpresaResponse;
+
+@Endpoint
+public class EmpresaEndpoint {
+    private static final String NAMESPACE = "/empresa";
+
+    private EmpresaRepository empresaRepository;
+
+    @Autowired
+    public EmpresaEndpoint(EmpresaRepository eRepository) {
+        this.empresaRepository = eRepository;
+    }
+
+    @PayloadRoot(localPart = "getEmpresaRequest", namespace = "/empresa")
+    @ResponsePayload
+    public GetEmpresaResponse getEmpresaRequest(@RequestPayload GetEmpresaRequest request) {
+        GetEmpresaResponse response = new GetEmpresaResponse();
+        response.setEmpresa(empresaRepository.findEmpresa(request.getId()));
+        return response;
+    }
+}
